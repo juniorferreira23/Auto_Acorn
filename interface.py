0947401dab1acefb72fcab5ee497b22a268e5185
juniorferreira23 <junior.ferreira@outlook.com>
2025-01-26 17:23:31 -0300

    refactor: adjustments to the executable windows

--- interface.py
+++ interface.py
@@ -2,12 +2,13 @@ import tkinter as tk
 import customtkinter as ctk
 from tkinter import filedialog, messagebox
 from dotenv import load_dotenv
+import threading
 import os
 from main import main
 
 load_dotenv()
 
-ctk.set_appearance_mode("System")  
+ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("blue")
 
 def browse_file(entry):
@@ -22,9 +23,20 @@ def browse_path(entry):
         entry.delete(0, tk.END)
         entry.insert(0, path)
 
-def run_interface(app):
-    
-    def on_start():
+def show_loading():
+    loading_window = ctk.CTkToplevel()
+    loading_window.title("Processando")
+    loading_window.geometry("300x100")
+    loading_window.resizable(False, False)
+    loading_window.grab_set()  # Impede interação com a janela principal enquanto processa
+
+    label = ctk.CTkLabel(loading_window, text="Processando, aguarde...", font=('Century Gothic bold', 14))
+    label.pack(pady=20)
+
+    return loading_window
+
+def run_process(entry_input, entry_output, loading_window):
+    try:
         path_input = entry_input.get()
         path_output = entry_output.get()
         if not path_input:
@@ -32,21 +44,28 @@ def run_interface(app):
         if not path_output:
             path_output = os.getenv('PATH_OUTPUTS')
         path_output = f'{path_output}/{os.getenv("PATH_OUTPUTS")}'
-        
-        messagebox.showinfo('Informação', 'Processo iniciado. Pode levar alguns minutos, dependendo do volume de dados.')
-        try:
-            url = os.getenv('URL')
-            main(path_input, path_output, url)
-            messagebox.showinfo('Informação', 'Processo concluído com sucesso.')
-        except Exception as e:
-            messagebox.showerror('Erro', f'Ocorreu um erro durante o processo:\n{e}')
-    
+
+        url = os.getenv('URL')
+        main(path_input, path_output, url)
+
+        messagebox.showinfo('Informação', 'Processo concluído com sucesso.')
+    except Exception as e:
+        messagebox.showerror('Erro', f'Ocorreu um erro durante o processo:\n{e}')
+    finally:
+        loading_window.destroy()
+
+def on_start(entry_input, entry_output):
+    loading_window = show_loading()
+    process_thread = threading.Thread(target=run_process, args=(entry_input, entry_output, loading_window))
+    process_thread.start()
+
+def run_interface(app):
     app.title("Extrator de Dados de CNPJs")
     app.geometry("600x350")
 
-    label_input = ctk.CTkLabel(app, text="Selecione o arquivo de entrada (planilha)", font=('Century Gothic bold', 14), )
+    label_input = ctk.CTkLabel(app, text="Selecione o arquivo de entrada (planilha)", font=('Century Gothic bold', 14))
     label_input.grid(row=0, column=0, sticky='w', padx=20, pady=(10, 0))
-    
+
     span_input = ctk.CTkLabel(app, text="(Se o arquivo estiver no caminho padrão, não é necessário selecionar)", font=('Century Gothic', 12))
     span_input.grid(row=1, column=0, sticky='w', padx=20, pady=(0, 10))
 
@@ -58,7 +77,7 @@ def run_interface(app):
 
     label_output = ctk.CTkLabel(app, text="Escolha o destino do arquivo de saída", font=('Century Gothic bold', 14))
     label_output.grid(row=3, column=0, sticky='w', padx=20, pady=(0, 0))
-    
+
     span_output = ctk.CTkLabel(app, text="(Se deseja salvar no caminho padrão, não é necessário selecionar)", font=('Century Gothic', 12))
     span_output.grid(row=4, column=0, sticky='w', padx=20, pady=(0, 10))
 
@@ -68,9 +87,8 @@ def run_interface(app):
     btn_output = ctk.CTkButton(app, text="Selecionar Pasta", command=lambda: browse_path(entry_output))
     btn_output.grid(row=5, column=1, sticky='w', padx=(0, 20), pady=(0, 40))
 
-    btn_start = ctk.CTkButton(app, text="Iniciar Processo", command=on_start, fg_color='#151', hover_color='green')
+    btn_start = ctk.CTkButton(app, text="Iniciar Processo", command=lambda: on_start(entry_input, entry_output), fg_color='#151', hover_color='green')
     btn_start.grid(row=6, column=0, sticky='w', padx=20, pady=(0, 10))
-
 
 if __name__ == "__main__":
     app = ctk.CTk()
